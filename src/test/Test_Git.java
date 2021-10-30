5524578b5169bc5a9a3506c14d4b7e0eb27fbd88
Phattarathida Punyarit <ptrtd.punyarit@gmail.com>
2021-10-30 21:51:39 +0700

    test git main

--- src/test/Test_Git.java
+++ src/test/Test_Git.java
@@ -5,6 +5,7 @@ public class Test_Git {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git Test");
+		System.out.println("Bracnh: Main");
 	}
 
 }
