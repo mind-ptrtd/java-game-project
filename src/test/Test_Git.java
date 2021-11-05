18b7e90c15c40a4a531a46330974fd61e6895efe
JinDamanee <punnapob.jdk@gmail.com>
2021-11-05 21:48:55 +0700

    Test Only On My branch

--- src/test/Test_Git.java
+++ src/test/Test_Git.java
@@ -5,15 +5,11 @@ public class Test_Git {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git Test");
-<<<<<<< HEAD
-		System.out.println("Branch : Main");
-||||||| 2feab6c
-		System.out.println("Bracnh: Main");
-=======
 		System.out.println("Bracnh: Main");
 		
 		System.out.println("Test From Earth^^");
->>>>>>> b1eba81a7b580132a905de218a748affb23cff17
+		
+		System.out.println("This is Only on My Branch");
 	}
 
 }
