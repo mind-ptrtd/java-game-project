cd2aa865289620bc02ddb92b9ee6ed6349375207
JinDamanee <punnapob.jdk@gmail.com>
2021-11-05 22:20:56 +0700

    Test Structure

---
+++ src/logic/TestLogic.java
@@ -0,0 +1,9 @@
+package logic;
+
+public class TestLogic {
+	private static int MAX_COIN;
+	
+ 	public static void Calculate() {
+ 		// Logic Here
+ 	}
+}
--- src/main/main.java
+++ src/main/main.java
@@ -1,6 +1,6 @@
-package test;
+package main;
 
-public class Test_Git {
+public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
