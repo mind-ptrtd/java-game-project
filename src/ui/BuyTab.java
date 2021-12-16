092fff405c9a241cead55c1ccdd5f843585bbc6a
Phattarathida Punyarit <ptrtd.punyarit@gmail.com>
2021-12-16 20:21:02 +0700

    update ui

--- src/ui/BuyTab.java
+++ src/ui/BuyTab.java
@@ -6,6 +6,10 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
@@ -20,22 +24,17 @@ public class BuyTab extends GridPane {
 		this.setHgap(20);
 		this.setPrefWidth(300);
 		this.setPrefHeight(100);
-		this.setBackground(new Background(new BackgroundFill(Color.LIGHTGRAY, CornerRadii.EMPTY, Insets.EMPTY)));
+		this.setLayoutX(260);
+		
+		this.setBorder(new Border(new BorderStroke(Color.AZURE, BorderStrokeStyle.SOLID, 
+				CornerRadii.EMPTY, new BorderWidths(4.5))));
+		this.setBackground(new Background(new BackgroundFill(Color.CADETBLUE, CornerRadii.EMPTY, Insets.EMPTY)));
 
 		BuyItem fishSpeed = new BuyItem("Fish Speed");
 		BuyItem hookSpeed = new BuyItem("Hook Speed");
 		BuyItem hookSize = new BuyItem("Hook Size");
 		BuyItem playerSpeed = new BuyItem("Player Speed");
 		buyItemList.addAll(fishSpeed, hookSpeed, hookSize, playerSpeed);
-
-//	 	for (BuyItem buyItem : buyItemList) {
-//	 		buyItem.setOnMouseClicked(new EventHandler<MouseEvent>() {
-//	 			//public void handle(ActionEvent event) 
-//	 				
-//	 			
-//	 				
-//	 		
-//	 	}
 
 		this.addRow(0, fishSpeed, hookSpeed, hookSize, playerSpeed);
 
